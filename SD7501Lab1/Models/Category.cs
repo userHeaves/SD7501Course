60ef07c0fbd7eb7bb6a9212a741dba6147681269
userHeaves <andrew.heavey@whitireianz.ac.nz>
2025-05-02 15:19:25 +1200

    Lab work

---
+++ SD7501Lab1/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SD7501Lab1.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Range(1,100,ErrorMessage = "Please enter between 1 and 100")]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
